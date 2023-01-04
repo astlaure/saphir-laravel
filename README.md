0d76c5b02aedd2ca26a31c37897e10ba77f1e8dd
Alexandre St-Laurent <alexandre.stlaurent.4@gmail.com>
2023-01-04 11:16:32 -0500

    initial commit 🎉

---
+++ README.md
@@ -0,0 +1,7 @@
+# Saphir Laravel
+
+## Optional packages
+
+1. laravel-lang/lang
+2. barryvdh/laravel-ide-helper
+3. barryvdh/laravel-debugbar
---
+++ src/SaphirServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Astlaure\Saphir;
+
+use Illuminate\Support\ServiceProvider;
+
+class SaphirServiceProvider extends ServiceProvider {
+    public function register() {
+        //
+    }
+
+    public function boot() {
+        //
+    }
+}
